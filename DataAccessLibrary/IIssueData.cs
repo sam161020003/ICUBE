6160a8c4e89330ecce0a3170c555b23a90bb207e
Samarth Singh Adhikari <sam_home@SAMs-Air.local>
2025-07-22 10:46:22 +0530

    Initial commit with full project

---
+++ DataAccessLibrary/IIssueData.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLibrary.Models;
+
+namespace DataAccessLibrary
+{
+    public interface IIssueData
+    {
+        Task<List<IssueModel>> GetIssues();
+        Task ApIssue(int id);
+        Task<List<IssueModel>> GetvIssues();
+        Task<List<IssueModel>> GetIssue(int id);
+        Task UpdateIssue(int id, IssueModel Issue);
+        Task InsertIssue(IssueModel Issue);
+        Task<List<IssueModel>> GetIndIssues(int id);
+
+	}
+}
